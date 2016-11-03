ee7d55d84c8c259f4cc69f96fcf38052eb34295e
Luke Wilson <wilson.palooza@gmail.com>
2016-11-03 13:54:56 -0700

    - Adds a deleteAllContentsByLessonId to content-route-handlers

--- api/routes/content-route-handlers.js
+++ api/routes/content-route-handlers.js
@@ -21,9 +21,31 @@ exports.createContent = (req, res) => {};
 
 exports.updateContentById = (req, res) => {
   const id = req.params.id;
+  var data = req.body;
+
+  Content.findOneAndUpdate({_id: id}, data, {new: true}, function(err, doc) {
+    if (err) {
+      log.error(err)
+    }
+    return doc;
+  });
+
+  res.status(201).send("updated a lesson");
 };
 
 exports.deleteContentById = (req, res) => {
   const id = req.params.id;
 };
+
+exports.deleteAllContentsByLessonId = (id) => {
+  console.log('deleteAllContentsByLessonId fired')
+  Content.remove({"lessonId": id}, function(err, doc) {
+    if (err) {
+      log.error(err);
+    }
+    return doc;
+  })
+};
+
+
 
