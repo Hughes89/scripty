d392a12b12cc29d16a6d2aba5b411517eb27c822
Alex Leo <aleo1337@gmail.com>
2016-11-04 15:38:09 -0400

    Add lesson into lessons completed, updates existing lessons if exists and score is higher than currently stored score

--- api/routes/user-route-handlers.js
+++ api/routes/user-route-handlers.js
@@ -45,9 +45,52 @@ exports.deleteUserById = (req, res) => {
 
 exports.addCompletedLesson = (req, res) => {
   const id = req.params.id;
-  const data = req.body;
-  User.findOneAndUpdate({_id: id}, {$push: {lessonsCompleted: data}}, {upsert: true, new: true}, (err, doc) => {
-    if (err) log.error(err);
-    res.status(201).send(doc);
+  const lessonId = req.body.lessonId;
+  const score = req.body.score;
+  User.findOne({_id: id}).then(function(user) {
+    user.lessonsCompleted.forEach(function(lesson) {
+      if (lesson.lessonId === lessonId) {
+        if (lesson.score < score) {
+          lesson.score = score;
+        }
+      }
+    });
+
+    var found = false;
+    for (var i = 0; i < user.lessonsCompleted.length; i++) {
+      if (user.lessonsCompleted[i].lessonId === lessonId) {
+        found = true;
+      }
+    }
+    if (!found) {
+      user.lessonsCompleted.push({score: score, lessonId: lessonId});
+    }
+
+    User.findOneAndUpdate({_id:id}, {lessonsCompleted: user.lessonsCompleted}, function(err) {
+     if (err) {
+       log.error(err);
+     }
+     res.status(201).send(user);
+    });
   });
+
+  // .save().then(function(user) {
+  //   res.status(201).send(user);
+  // });
+
+
+  // User.findOneAndUpdate({_id: id}, {$addToSet: {lessonsCompleted: data}}, {upsert: true, new: true}, (err, doc) => {
+  //   if (err) log.error(err);
+  //   res.status(201).send(doc);
+  // });
 };
+
+
+
+
+
+
+
+
+
+
