b05ada732d5200bb2c68e6bc6bd775aba5e3d589
Luke Wilson <wilson.palooza@gmail.com>
2016-11-06 13:13:26 -0800

    - Creates hashingHelpers to hash password and comparing password

---
+++ api/helpers/hashingHelpers.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt-nodejs');
+const Promise = require('bluebird');
+
+exports.hashPassword = function (password, model) {
+  return new Promise(function(resolve,reject){
+    bcrypt.hash(password, null, null, function(err, result) {
+      if (err) return reject (err);
+      else {
+        // model.set('password', 'result');
+        resolve(result);
+      }
+    });
+  })
+  .then(function(result) {
+    return result;
+  });
+};
+
+exports.comparePassword = (hash, userGivenPassword) => {
+  console.log('comparing password');
+  return new Promise(function(resolve, reject){
+    bcrypt.compare(userGivenPassword, hash, function(err, result){
+      if(err) return reject(err);
+      else {
+        resolve(result);
+      }
+    });
+  });
+};
